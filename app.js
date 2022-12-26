5b0eb0bd777e1b0f8c725b71e3ae5e55056e2b28
venkat3534 <84139526+venkat3534@users.noreply.github.com>
2022-12-26 14:56:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-/*
+const express = require('express')
+const app = express()
+const port = 3000
 
-GEREKLİ PAKETLER YÜKLENİYOR...
 
-*/
-var http = require('http');
-var express = require('express');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-var app = express();
 
-app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
-app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
-app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
-app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
-
-require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
-
-/*
-
-HTTP SERVER OLUŞTURULDU
-
-*/
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyor.');
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
